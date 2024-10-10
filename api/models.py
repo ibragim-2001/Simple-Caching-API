da5694d1224c542a4e898cc01ba47a3d2931068d
ibragim-2001 <saitamadesign7@gmail.com>
2024-10-10 21:28:32 +0300

    add models, serializers, app urls

--- api/models.py
+++ api/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Title')
+    description = models.TextField(max_length=255, verbose_name='Description')
+
+    class Meta:
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+
+    def __str__(self):
+        return self.title
