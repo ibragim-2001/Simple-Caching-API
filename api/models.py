9b4200ea194cae4985ed0c11020a990df5ef5262
ibragim-2001 <saitamadesign7@gmail.com>
2024-10-10 21:31:28 +0300

    rename model

--- api/models.py
+++ api/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 
-class Course(models.Model):
+class Items(models.Model):
     title = models.CharField(max_length=255, verbose_name='Title')
     description = models.TextField(max_length=255, verbose_name='Description')
 
     class Meta:
-        verbose_name = 'Course'
-        verbose_name_plural = 'Courses'
+        verbose_name = 'Item'
+        verbose_name_plural = 'Items'
 
     def __str__(self):
         return self.title
