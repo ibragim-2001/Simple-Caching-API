227fa3063e4222c700fe4495bcbb39fc78d1515a
ibragim-2001 <saitamadesign7@gmail.com>
2024-10-10 23:14:06 +0300

    add urls, serializer, models, migrations, views

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import ItemsView
+
+
+urlpatterns = [
+    path('items/', ItemsView.as_view())
+]
--- api/views.py
+++ api/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+
+from .serializers import ItemsSerializer
+from .models import Items
+
+
+class ItemsView(APIView):
+
+    def get(self, request):
+        try:
+            items = Items.objects.all()
+        except Exception as exception:
+            return Response({'message': str(exception)})
+        serializer = ItemsSerializer(items, many=True)
+        return Response(data=serializer.data, status=HTTP_200_OK)
